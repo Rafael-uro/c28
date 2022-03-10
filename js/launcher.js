fcc2d87e85811a82e59840f0ea194dfd4fce1516
Rafael-uro <94479855+Rafael-uro@users.noreply.github.com>
2022-03-09 22:45:37 -0300

    Add files via upload

---
+++ js/launcher.js
@@ -0,0 +1,36 @@
+class launcher {
+    constructor(bodyA, pointB) {
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.launcher = Constraint.create(options);
+        World.add(world, this.launcher)
+        this.pointB = pointB
+    }
+    fly() {
+        this.launcher.bodyA = null
+        if (this.launcher.bodyA) {
+            strokeWeight(4)
+            line(pointA.x, pointA.y, pointB.x, pointB.y)
+        }
+
+    }
+
+
+    display() {
+        var pointA = this.launcher.bodyA.position
+        var pointB = this.pointB
+        strokeWeight(4)
+        line(pointA.x, pointA.y, pointB.x, pointB.y)
+
+
+
+
+    }
+
+
+
+}
